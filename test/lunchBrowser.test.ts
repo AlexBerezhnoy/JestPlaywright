4add7bc93e4e48304ead8b6fa888a1ef24d44c9d
Oleksandr Berezhniy <dinnylol221@gmail.com>
2022-04-09 17:28:13 +0300

    first commit

---
+++ test/lunchBrowser.test.ts
@@ -0,0 +1,10 @@
+import { chromium } from "playwright";
+describe('Lunch Browser', () => {
+    test('Open letcode', async () => {
+        const browser = await chromium.launch({headless:false});
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        await page.goto('https://example.com');
+        await browser.close();
+    })
+})
