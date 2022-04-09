dca06e4639584c7a8e1dc7482f20fb35c9ad51d9
Oleksandr Berezhniy <dinnylol221@gmail.com>
2022-04-09 17:47:12 +0300

    edit home url

--- test/lunchBrowser.test.ts
+++ test/lunchBrowser.test.ts
@@ -1,10 +1,10 @@
 import { chromium } from "playwright";
-describe('Lunch Browser', () => {
+describe('Login ', () => {
     test('Open letcode', async () => {
         const browser = await chromium.launch({headless:false});
         const context = await browser.newContext();
         const page = await context.newPage();
-        await page.goto('https://example.com');
+        await page.goto('https://letcode.in/');
         await browser.close();
     })
 })
